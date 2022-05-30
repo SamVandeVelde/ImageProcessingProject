4a393ad5bc0503cb047ba3c7f100aaa79b9c24e1
aheirman <48590738+aheirman@users.noreply.github.com>
2022-05-30 13:09:24 +0200

    remove gitcache add work

--- combination_algos.py
+++ combination_algos.py
@@ -1,20 +1,34 @@
 from enum import Enum
 import math
+import numpy as np
+
+
 
 class ALGO(Enum):
-    NO_REJECTION,
-    MEDIAN,
-    MINMAX,
-    SIGMA_CLIPPING,
-    AVG_SIGMA_CLIPPING,
-    NO_WEIGHTING_NO_REJECT,
-    TURKEYS_BIWEIGHT
+    NO_REJECTION = 1,
+    MEDIAN = 2,
+    MINMAX = 3,
+    SIGMA_CLIPPING = 4,
+    AVG_SIGMA_CLIPPING = 5,
+    NO_WEIGHTING_NO_REJECT = 6,
+    TURKEYS_BIWEIGHT = 7
 
 def combination_alogs(rgb_vec, algo):
     match algos:
         case ALGO.NO_REJECTION:
             # aron
-            return []
+            x_len = len(rgb_vec[0])
+            y_len = len(rgb_vec[0][0])
+            i_len = len(rgb_vec)
+            ret = np.zeros([x_len, y_len])
+            for x in range(x_len):
+                for y in range(y_len):
+                    avg = []
+                    for index in range(i_len):
+                        avg += rgb_vec[index][x][y]
+                    avg = avg/i_len
+                    ret[x,y] = avg
+            return ret
         case ALGO.MEDIAN:
             # Louis
             return None
@@ -33,5 +47,5 @@ def combination_alogs(rgb_vec, algo):
         case ALGO.TURKEYS_BIWEIGHT:
             # aron
             return None
-        case _
+        case _:
             assert False
