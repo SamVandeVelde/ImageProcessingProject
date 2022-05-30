5206f62a5ee74baa23d3fbaf89d15a82c6322040
aheirman <48590738+aheirman@users.noreply.github.com>
2022-05-30 11:32:33 +0200

    For my friend

--- aron_main.py
+++ aron_main.py
@@ -1,9 +1,14 @@
+import rawpy
+from matplotlib import pyplot as plt
 
 
 def aron_main():
-    path = 'image.nef'
+    path = './data/Darks/Darks1.NEF'
     with rawpy.imread(path) as raw:
         rgb = raw.postprocess()
+        print(rgb)
+        plt.imshow(rgb, interpolation='nearest')
+        plt.show()
 
 
 aron_main()
