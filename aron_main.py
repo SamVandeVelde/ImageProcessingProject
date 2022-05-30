a0fb732212ca7ccae0c7e7ea86b7d360b7ae600f
aheirman <48590738+aheirman@users.noreply.github.com>
2022-05-30 19:27:25 +0200

    fix

--- aron_main.py
+++ aron_main.py
@@ -11,8 +11,10 @@ def aron_main():
     #path = './data/Darks/Darks1.NEF'
     
     #base   = './data/Lights/darktable_exported/Lights'
-    base   = './data2/Darks/Darks'
-    length = 12
+    #base   = './data2/Darks/Darks'
+    base   = './data2/lights/Lights'
+    path_dark   = './data2/DARK_12_FULL_RES.PNG'
+    length = 1
     paths  = [f'{base}{i}.jpg' for i in range(1,length+1)]
     
     rgb_vec = []
@@ -22,6 +24,12 @@ def aron_main():
         rgb_vec.append(rgb)
 
     rgb = combination_alogs(rgb_vec, ALGO.NO_REJECTION)
+
+    img    = Image.fromarray(rgb)
+    img.show()
+
+    dark   =  imageio.imread(path_dark)
+    rgb   = np.abs(rgb - dark[0:2848,0:4316,:])
 
     img    = Image.fromarray(rgb)
     img.show()
