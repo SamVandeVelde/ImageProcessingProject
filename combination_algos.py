5255d6fcf4ca77600ff7ae4020861025db9cb268
aheirman <48590738+aheirman@users.noreply.github.com>
2022-05-30 12:21:42 +0200

    label parts

--- combination_algos.py
+++ combination_algos.py
@@ -12,19 +12,26 @@ class ALGO(Enum):
 
 def combination_alogs(rgb_vec, algo):
     match algos:
-        case ALGO.NO_REJECTION,:
+        case ALGO.NO_REJECTION:
+            # aron
             return []
         case ALGO.MEDIAN:
+            # Louis
             return None
         case ALGO.MINMAX:
+            # aron
             return None
         case ALGO.SIGMA_CLIPPING:
+            # Louis
             return None
         case ALGO.AVG_SIGMA_CLIPPING:
+            # aron
             return None
         case ALGO.NO_WEIGHTING_NO_REJECT:
+            # Louis
             return None
         case ALGO.TURKEYS_BIWEIGHT:
+            # aron
             return None
         case _
             assert False
