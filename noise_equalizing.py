349f99e718394df1c3e0eaa2db10a9175e5daf46
Louis-Van-Eeckhoudt <62985294+Louis-Van-Eeckhoudt@users.noreply.github.com>
2022-05-30 17:42:38 +0200

    hist stretch final

--- noise_equalizing.py
+++ noise_equalizing.py
@@ -4,12 +4,9 @@ from PIL import Image
 
 
 def noise_equal(rgb):
-    # each picture is histogram stretched
-    print(rgb.shape)
+    # picture is histogram stretched
     x_max = rgb.max()
     x_min = rgb.min()
-    print(x_min)
-    print(x_max)
     temp = np.round(255*((rgb - x_min)/(x_max - x_min)))
     ret = temp.astype('uint8')
     return ret
