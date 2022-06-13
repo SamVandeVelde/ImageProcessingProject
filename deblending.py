3468ee81ad110c6fa786109cb70caad7d64780c4
SamVandeVelde <58397500+SamVandeVelde@users.noreply.github.com>
2022-06-13 22:13:58 +0200

    I forgot to commit an important bit UwU

--- deblending.py
+++ deblending.py
@@ -2,7 +2,7 @@ import math
 import numpy as np
 
 
-def get_a11(instr_beamwidth):
+def get_a11(instr_beamwidth, target_subindex, nontarget_subindex, locations):
     outer_sum = 0
     for j in target_subindex:
         for k in target_subindex:
@@ -10,7 +10,7 @@ def get_a11(instr_beamwidth):
     return outer_sum
 
 
-def get_a12(instr_beamwidth):
+def get_a12(instr_beamwidth, target_subindex, nontarget_subindex, locations):
     outer_sum = 0
     for j in target_subindex:
         for k in nontarget_subindex:
@@ -18,7 +18,7 @@ def get_a12(instr_beamwidth):
     return outer_sum
 
 
-def get_a21(instr_beamwidth):
+def get_a21(instr_beamwidth, target_subindex, nontarget_subindex, locations):
     outer_sum = 0
     for j in nontarget_subindex:
         for k in target_subindex:
@@ -26,7 +26,7 @@ def get_a21(instr_beamwidth):
     return outer_sum
 
 
-def get_a22(instr_beamwidth):
+def get_a22(instr_beamwidth, target_subindex, nontarget_subindex, locations):
     outer_sum = 0
     for j in nontarget_subindex:
         for k in nontarget_subindex:
@@ -100,10 +100,10 @@ def deblending(locations, meas_flux, target_positions, instr_beamwidth):
 
     # to solve: Ax = b
     A = np.zeros((2, 2))
-    A[0, 0] = get_a11(instr_beamwidth)
-    A[0, 1] = get_a12(instr_beamwidth)
-    A[1, 0] = get_a21(instr_beamwidth)
-    A[1, 1] = get_a22(instr_beamwidth)
+    A[0, 0] = get_a11(instr_beamwidth, target_subindex, nontarget_subindex, locations)
+    A[0, 1] = get_a12(instr_beamwidth, target_subindex, nontarget_subindex, locations)
+    A[1, 0] = get_a21(instr_beamwidth, target_subindex, nontarget_subindex, locations)
+    A[1, 1] = get_a22(instr_beamwidth, target_subindex, nontarget_subindex, locations)
     B = np.zeros((2, 1))
     B[0, 0] = sum(target_meas_flux)
     B[1, 0] = sum(nontarget_meas_flux)
@@ -112,40 +112,3 @@ def deblending(locations, meas_flux, target_positions, instr_beamwidth):
     I0 = x[0]
     I1 = x[1]
     return I0, I1
-
-# locations = [[1, 2], [3, 4], [5, 6], [0, 2], [1, 1], [7, 8]]  # TODO GET ACTUAL LOCATIONS
-# meas_flux = [5, 11, 12, 4, 2, 9]  # TODO GIVE ME THE INTENSITIES!!!
-# target_positions = [1, 0, 0, 1, 0, 1]
-# am_known_objects = len(locations)
-# target_subindex = []
-# nontarget_subindex = []
-# for i in range(am_known_objects):
-#    if target_positions[i] == 1:
-#        target_subindex.append(i)
-#    else:
-#        nontarget_subindex.append(i)
-
-# target_locations = []
-# nontarget_locations = []
-# target_meas_flux = []
-# nontarget_meas_flux = []
-# for i in target_subindex:
-#    target_locations.append(locations[i])
-#    target_meas_flux.append(meas_flux[i])
-# for i in nontarget_subindex:
-#    nontarget_locations.append(locations[i])
-#    nontarget_meas_flux.append(meas_flux[i])
-
-# to solve: Ax = b
-# A = np.zeros((2, 2))
-# A[0, 0] = get_a11()
-# A[0, 1] = get_a12()
-# A[1, 0] = get_a21()
-# A[1, 1] = get_a22()
-# B = np.zeros((2, 1))
-# B[0, 0] = sum(target_meas_flux)
-# B[1, 0] = sum(nontarget_meas_flux)
-
-# x = np.linalg.solve(A, B)
-# I0 = x[0]
-# I1 = x[1]
