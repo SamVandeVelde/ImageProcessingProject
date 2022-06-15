bf6966fbd8db0d7f5a2b3c8cdb80e8f73d1ee0ac
Louis-Van-Eeckhoudt <62985294+Louis-Van-Eeckhoudt@users.noreply.github.com>
2022-06-15 17:04:47 +0200

    NWNR fix

--- combination_algos.py
+++ combination_algos.py
@@ -256,9 +256,9 @@ def combination_alogs(rgb_vec, algo):
             return ret
         case ALGO.NO_WEIGHTING_NO_REJECT:
             # Louis
-            x_len = len(rgb_vec[0])  # // 8
+            x_len = len(rgb_vec[0])   // 8
             print(x_len)
-            y_len = len(rgb_vec[0][0])  # // 1
+            y_len = len(rgb_vec[0][0])   // 8
             print(y_len)
             i_len = len(rgb_vec)
             # print(f'shape pre: {rgb_vec[0].shape}, {rgb_vec[0].dtype}')
@@ -271,7 +271,7 @@ def combination_alogs(rgb_vec, algo):
 
             ret = np.concatenate([res.get(timeout=1000) for res in results])
             ret = noise_equal(ret)
-            return None
+            return ret
         case ALGO.TURKEYS_BIWEIGHT:
             # SAM
             x_len = len(rgb_vec[0]) // 2
